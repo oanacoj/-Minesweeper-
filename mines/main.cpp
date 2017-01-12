39dc17c5941afbd554f608801ef041e1242ce203
oanacoj <c.oana98@gmail.com>
2017-01-12 18:07:42 +0200

    update

--- mines/main.cpp
+++ mines/main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <time.h>
-
+#include <stdlib.h>
 
 using namespace std;
 
-
-int matrix (unsigned int n,unsigned int nrBombe,int mapa[][])
+int matrix (unsigned int nrBombe,int mapa[10][10])
 {
+    srand(time(NULL));
     unsigned int i, j;
-    for( i=0; i<n; i++)
+    for( i=0; i<10; i++)
     {
-        for( j=0; j<n; j++)
+        for( j=0; j<10; j++)
             mapa[i][j]=0;
     }
     int bombeAdd=0;
     while(bombeAdd<nrBombe)
     {
-        i=random(0, n);
-        j=random(0,n);
-        if(mapa[i][j]==0)
+        i=rand() % 10;
+        j=rand() % 10;
+        if(mapa[i][j] != -1)
         {
             mapa[i][j]=-1;
             bombeAdd++;
@@ -33,12 +33,12 @@ int matrix (unsigned int n,unsigned int nrBombe,int mapa[][])
             if(mapa[i][j-1]>=0)
                 mapa[i][j-1]++;
         }
-        if(i<n-1)
+        if(i<9)
         {
             if(mapa[i+1][j]>=0)
                 mapa[i+1][j]++;
         }
-        if(j<n-1)
+        if(j<9)
         {
             if(mapa[i][j+1]>=0)
                 mapa[i][j+1]++;
@@ -48,25 +48,26 @@ int matrix (unsigned int n,unsigned int nrBombe,int mapa[][])
                 if(mapa[i-1][j-1]>=0)
                     mapa[i-1][j-1]++;
             }
-        if(i>0 && j<n-1)
+        if(i>0 && j<9)
             {
                 if(mapa[i-1][j+1]>=0)
                     mapa[i-1][j+1]++;
             }
-        if(i<n-1 && j>0)
+        if(i<9 && j>0)
         {
             if(mapa[i+1][j-1]>=0)
                 mapa[i+1][j-1]++;
         }
-        if(i<n-1 && j<n-1)
+        if(i<9 && j<9)
         {
             if(mapa[i+1][j+1]>=0)
                 mapa[i+1][j+1]++;
         }
     }
+    return mapa[10][10];
 }
 
-int fcSelect(unsigned int i,unsigned int j,unsigned int n, int mapa[][],int cover[][])
+int fcSelect(unsigned int i,unsigned int j, int mapa[10][10],int cover[10][10])
 {
     if(mapa[i][j]== -1)
         return -1;
@@ -80,22 +81,22 @@ int fcSelect(unsigned int i,unsigned int j,unsigned int n, int mapa[][],int cove
         if(mapa[i][j-1] != -1)
             cover[i][j-1] = 1;
     }
-    if(i<n-1)
+    if(i<9)
     {
         if(mapa[i+1][j] != -1)
             cover[i+1][j]=1;
     }
-    if(j<n-1)
+    if(j<9)
     {
         if(mapa[i][j+1] != -1)
             cover[i][j+1]=1;
     }
-    return cover;
+    return cover[10][10];
 }
 
 
 
-int selectC(unsigned int i,unsigned int j,unsigned int n, int mapa[][],int cover[][])
+int selectC(unsigned int i,unsigned int j, int mapa[10][10],int cover[10][10])
 {
     if(mapa[i][j]==-1)
         return -1;
@@ -111,41 +112,41 @@ int selectC(unsigned int i,unsigned int j,unsigned int n, int mapa[][],int cover
             selectC(i-1, j, mapa, cover);
         if(j>0 && cover[i][j-1] != 1 && mapa[i][j-1]!= -1)
             selectC(i, j-1, mapa, cover);
-        if(i<n-1 && cover[i+1][j] != 1 && mapa[i+1][j]!=-1)
+        if(i<9 && cover[i+1][j] != 1 && mapa[i+1][j]!=-1)
             selectC(i+1, j, mapa, cover);
-        if(j<n-1 && cover[i][j+1]!=1 && mapa[i][j+1]!=-1)
+        if(j<9 && cover[i][j+1]!=1 && mapa[i][j+1]!=-1)
             selectC(i, j+1, mapa, cover);
     }
-    return cover[][];
+    return cover[10][10];
 }
 
 
 
-void afisare(unsigned int n, int mapa[][],int cover[][])
+void afisare(int mapa[10][10],int cover[10][10])
 {
     unsigned int i, j;
-    for(i=0; i<n; i++)
+    for(i=0; i<10; i++)
     {
-        for(j=0; j<n; j++)
+        for(j=0; j<10; j++)
             if(cover[i][j]==1)
         {
             cout<<mapa[i][j];
         }
             else
-                cout<<" ";
+                cout<<"| |";
             cout<<endl;
     }
 }
 
 
 
-int getScor(int cover[][],int n)
+int getScor(int cover[10][10],int n)
 {
     unsigned int i, j;
     int scor=0;
-    for(i=0; i<n; i++)
+    for(i=0; i<10; i++)
     {
-        for(j=0; j<n; j++)
+        for(j=0; j<10; j++)
             if(cover[i][j]==1)
             scor++;
     }
@@ -153,11 +154,15 @@ int getScor(int cover[][],int n)
 }
 
 
-int main
+int main ()
 {
-    int mapa[][];
-    mapa=matrix(n, nrBombe, mapa[][]);
+    int cover[10][10], mapa[10][10];
+    unsigned int n;
+    cout << "dati nr bombe:";
+    cin >> n;
+    matrix(n, mapa);
     int alive=1;
+    int i, j, x, scor;
     while(alive)
     {
         afisare(mapa, cover);
@@ -168,11 +173,11 @@ int main
             cout<<"j=";
             cin>>j;
         }
-        while(i<0 || i>n || j<0 || j>n);
-        x=selectC(i, j, mapa, cover)
+        while(i<0 || i>10 || j<0 || j>10);
+        x=selectC(i, j, mapa, cover);
         if(x==-1)
             alive=0;
     }
-    scor=getScor(cover, n);
+    scor=getScor(cover, 10);
     cout<<scor;
 }
